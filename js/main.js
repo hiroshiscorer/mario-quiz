358766290d98e7e36a97853bf5e6531508087baf
TRA_Scorer <luis@zyp.mx>
2023-05-17 11:43:52 -0600

    First commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+	$('#click-next').on('click',function (){
+		$('#respuesta').css('opacity', '1');
+		$(this).css({
+			"opacity":"0",
+			"cursor":"initial"
+		});
+		$('#click-close').css("display", "block");
+		setTimeout(function (){
+			$('#click-close').css("opacity", "1");
+		}, 100);
+
+	});
+
+	$('#click-close').on('click', function (){
+		$('#pregunta-box').removeClass('active');
+		$('#respuesta').css('opacity', '0');
+		setTimeout(function (){
+			$('#click-next').css({
+				"opacity":"1",
+				"cursor":"pointer"
+			});
+			$('#click-close').css({
+				"opacity":"0",
+				"display":"none"
+			});
+		}, 1100);
+
+	});
+
+});
+
+function launch(who) {
+	console.log(who);
+
+	$('#pregunta').html(qa[who].q);
+	$('#respuesta').html(qa[who].a);
+	$('#pregunta-imagen').attr('src', 'images/'+who+'.png');
+	$('#pregunta-box').addClass('active');
+}
+
+const qa = {
+	nabbit: {
+		q: "CÓMO FUNCIONA EL MUNDO. En la actividad del recorrido al huerto de kinder, ¿Qué sentidos utilizaste?",
+		a: "Vista y olfato",
+	},
+	mario: {
+		q: "HOW THE WORLD WORKS. Which are the three states of matter?",
+		a: "Solid, liquid and gas.",
+	},
+	luigi: {
+		q: "DÓNDE NOS ENCONTRAMOS EN EL TIEMPO Y EL ESPACIO. ¿Qué es una reliquia?",
+		a: "PENDING_ANSWER"
+	},
+	question: {
+		q: "WHERE WE ARE IN PLACE AND TIME. In the interviews to grandparents, we observed things that can show both... ?",
+		a: "Change and continuity"
+	},
+	koopa: {
+		q: "QUIENES SOMOS. ¿Qué es una tradición y menciona algunas?",
+		a: "PENDING_ANSWER"
+	},
+	coins: {
+		q: "WHO WE ARE. What is a community?",
+		a: "A group of people living or working together."
+	},
+	lakitul: {
+		q: "CÓMO NOS ORGANIZAMOS. ¿Cuál es la diferencia entre ofrecer un producto y un servicio?",
+		a: "PENDING_ANSWER"
+	},
+	bobombs: {
+		q: "HOW WE ORGANIZE OURSELVES. What is the difference between buying and bartering?",
+		a: "When you buy you exchange for money, when you barter you exchange for other objects."
+	},
+	muncher: {
+		q: "CÓMO NOS EXPRESAMOS. Menciona algunos medios de comunicación.",
+		a: "Televisión, radio, streaming"
+	},
+	piranha: {
+		q: "HOW WE EXPRESS OURSELVES.  Which are the two types of messages we are exposed to?",
+		a: "Positive and negative messages."
+	},
+	goomba: {
+		q: "CÓMO COMPARTIMOS EL PLANETA. ¿Cuáles son las tres R?",
+		a: "Reducir, reutilizar y reciclar."
+	},
+	peachette: {
+		q: "SHARING THE PLANET. How do we call the process of rain falling to the ground and evaporating again into the sky?",
+		a: "The water cycle."
+	}
+};
