74f505cb3bc5503407e5618c6c7a23e40eb70b77
TRA_Scorer <luis@zyp.mx>
2023-05-19 19:56:06 -0600

    Question corrections

--- js/main.js
+++ js/main.js
@@ -26,7 +26,6 @@ $(document).ready(function(){
 				"display":"none"
 			});
 		}, 1100);
-
 	});
 
 });
@@ -43,7 +42,7 @@ function launch(who) {
 const qa = {
 	nabbit: {
 		q: "CÓMO FUNCIONA EL MUNDO. En la actividad del recorrido al huerto de kinder, ¿Qué sentidos utilizaste?",
-		a: "Vista y olfato",
+		a: "Vista y olfato.",
 	},
 	mario: {
 		q: "HOW THE WORLD WORKS. Which are the three states of matter?",
@@ -51,15 +50,15 @@ const qa = {
 	},
 	luigi: {
 		q: "DÓNDE NOS ENCONTRAMOS EN EL TIEMPO Y EL ESPACIO. ¿Qué es una reliquia?",
-		a: "PENDING_ANSWER"
+		a: "Objeto antiguo que tiene un valor sentimental para la familia."
 	},
 	question: {
 		q: "WHERE WE ARE IN PLACE AND TIME. In the interviews to grandparents, we observed things that can show both... ?",
-		a: "Change and continuity"
+		a: "Change and continuity."
 	},
 	koopa: {
 		q: "QUIENES SOMOS. ¿Qué es una tradición y menciona algunas?",
-		a: "PENDING_ANSWER"
+		a: "Son eventos donde participa toda la comunidad porque forman parte de nuestra cultura como día de muertos, reyes magos o navidad."
 	},
 	coins: {
 		q: "WHO WE ARE. What is a community?",
@@ -67,7 +66,7 @@ const qa = {
 	},
 	lakitul: {
 		q: "CÓMO NOS ORGANIZAMOS. ¿Cuál es la diferencia entre ofrecer un producto y un servicio?",
-		a: "PENDING_ANSWER"
+		a: "Los productos se entregan, como ropa, comida o útiles. Los servicios se prestan, como lo hace un doctor o una maestra."
 	},
 	bobombs: {
 		q: "HOW WE ORGANIZE OURSELVES. What is the difference between buying and bartering?",
@@ -75,11 +74,11 @@ const qa = {
 	},
 	muncher: {
 		q: "CÓMO NOS EXPRESAMOS. Menciona algunos medios de comunicación.",
-		a: "Televisión, radio, streaming"
+		a: "Televisión, radio, streaming."
 	},
 	piranha: {
-		q: "HOW WE EXPRESS OURSELVES.  Which are the two types of messages we are exposed to?",
-		a: "Positive and negative messages."
+		q: "HOW WE EXPRESS OURSELVES.  Name a form of expression.",
+		a: "Painting, singing, dancing, writing, etc."
 	},
 	goomba: {
 		q: "CÓMO COMPARTIMOS EL PLANETA. ¿Cuáles son las tres R?",
